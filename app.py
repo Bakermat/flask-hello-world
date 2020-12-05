7c957af0c798ea93b603868930f95c2305d21b55
Patrick Peeters <12652989+Bakermat@users.noreply.github.com>
2020-12-05 23:50:19 +1100

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def root():
-    return f'Hello World!', 200
+    return f'Welcome to our website!!', 200
 
 @app.route('/ip', methods=["GET"])
 def get_my_ip():
     return f'Your IP is: {request.remote_addr}', 200
 
+@app.route('/store', methods=["GET"])
+def get_my_ip():
+    return f'Welcome to the store!', 200
+
 @app.route('/error', methods=["GET"])
 def error():
-    return f'Fake Auth Error', 502
+    return f'Fake Authentication Error!', 502
